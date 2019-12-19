4297ab362ad964d1a440aa3d3d415e1ffe051f12
Niranjan Thrineshwara <NTH092@crb.apmoller.net>
2019-12-19 15:43:55 +0530

    initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <div>{{ count }}</div>
+    <div>{{ doneTodosCount }}</div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState(["count"]),
+    ...mapGetters(["doneTodos", "doneTodosCount", "getTodoById"]),
+    doneTodosCount() {
+      return this.$store.getters.doneTodosCount;
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
-import store from "./store";
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    todos: [
+      { id: 1, text: "abc", done: true },
+      { id: 2, text: "def", done: true }
+    ]
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    getTodoById: state => id => {
+      return state.todos.find(todo => todo.id === id);
+    }
+  }
+});
+
+store.commit("increment");
+// eslint-disable-next-line no-console
+console.log(store.state.count);
+// eslint-disable-next-line no-console
+console.log(store.getters.doneTodosCount);
+// eslint-disable-next-line no-console
+console.log(store.getters.getTodoById(2));
 
 new Vue({
   store,
